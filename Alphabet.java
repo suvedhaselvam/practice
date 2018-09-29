a89571b393b271b4fbc45e3cf774eb874587d4c7
suvedhaselvam <43680378+suvedhaselvam@users.noreply.github.com>
2018-09-29 21:05:31 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,1 +1,23 @@
+/* package whatever; // don't place package name! */
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		Character ch=s.next().charAt(0);
+		if(Character.isLetter(ch))
+		{
+			System.out.print("Alphabet");
+		}
+		else
+		{
+			System.out.print("No");
+		}
+	}
+}
